969649cdeab1c9925cb68217037a7d2fc230d005
Krzysztof Wilgucki <128691376+BrightIndigo@users.noreply.github.com>
2025-03-11 21:58:16 +0100

    Add files via upload

--- timer.py
+++ timer.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QVBoxLayout
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QMainWindow, QPushButton, QVBoxLayout
 from PyQt6.QtCore import QSize, Qt, QTimer
 import time
 
@@ -10,13 +10,15 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Timer")
         self.setFixedSize(QSize(400, 300))
         layout = QVBoxLayout()
-        start_button = QPushButton("Start timer!")
-        start_button.setCheckable(True)
-        start_button.clicked.connect(self.button_clicked)
-        self.timer = QTimer()
-        self.timer.timeout.connect(self.update)
-        self.timer.start(1000)
-        widgets = [start_button]
+        self.start_button = QPushButton("Start timer!")
+        self.start_button.setCheckable(True)
+        self.start_button.clicked.connect(self.button_clicked)
+        self.label = QLabel('Timer not started')
+        self.seconds = 0
+        self.minutes = 0
+        self.hours = 0
+        
+        widgets = [self.start_button, self.label]
 
         for widget in widgets:
             layout.addWidget(widget)
@@ -25,9 +27,46 @@ class MainWindow(QMainWindow):
         widget.setLayout(layout)
         self.setCentralWidget(widget)
 
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timer_start)
+
     def button_clicked(self, started):
-        self.started = started
-        print("Started?", self.started)
+        if started:
+            self.label.setText('Timer started')
+            self.timer.start(1000)
+            self.start_button.setText("Stop timer!")
+        else:
+            self.timer.stop()
+            self.start_button.setText("Start timer!")
+    
+    def timer_start(self):
+        self.seconds += 1
+        if self.seconds == 60:
+            self.minutes += 1
+            self.seconds = 0
+        if self.minutes == 60:
+            self.hours += 1
+            self.minutes = 0
+
+        seconds = "00"
+        minutes = "00"
+        hours = "00"
+
+        if self.seconds < 10:
+            seconds = "0"+str(self.seconds)
+        else:
+            seconds = str(self.seconds)
+        if self.minutes < 10:
+            minutes = "0"+str(self.minutes)
+        else:
+            minutes = str(self.minutes)
+        if self.hours < 10:
+            hours = "0"+str(self.hours) 
+        else:
+            hours = str(self.hours)
+
+        self.label.setText(f'{hours}:{minutes}:{seconds}')
+
 
 app = QApplication(sys.argv)
 window = MainWindow()
